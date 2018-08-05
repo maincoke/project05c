bc432afa6f29236a8181972691879887fba33474
Maincoke <maincoke@gmail.com>
2018-08-05 12:38:14 -0500

    Rediseño de header, adicion de efectos, inclusion de imagenes y articulos

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/**
+ *  --- Inicializacion de componentes de Materialize --- **
+ */
+document.addEventListener('DOMContentLoaded', function() {
+  var el01 = document.querySelectorAll('.sidenav');
+  var instance01 = M.Sidenav.init(el01, { edge: "right" })
+});
